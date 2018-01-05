3a4fc9b2f84225293085dc8f932e8a55418a070a
fcu-d0441320 <d0441320@fcu.edu.tw>
2018-01-06 07:21:53 +0800

    Add find movie theater near by me

--- ClientHandler.py
+++ ClientHandler.py
@@ -1,4 +1,7 @@
 from Messenger import MyMessenger
+from MovieTheater import *
+movietheater = MovieTheater()
+
 class ClientHandler:
     def __init__(self):
         self.client={}
@@ -62,6 +65,31 @@ class Client:
     def status11(self):
         self.Messenger.setText('查詢中請稍後...')
         self.Messenger.send()
+        near = movietheater.findWhoIsNearToMe(self.location[0],self.location[1])
+        if len(near)==0:
+            self.Messenger.setText('抱歉!\n我在附近找不到電影院...')
+            self.Messenger.send()
+        else:
+            place=[False,False,False]
+            for ele in near:
+                if ele[1]=='賓':
+                    place[1]=True
+                elif ele[2]=='新':
+                    place[2]=True
+                else:
+                    place[0]=True
+            txt="我在附近找到"+str(len(near))+"間電影院\n你想去哪一家呢?"
+            print(near)
+            self.Messenger.setText(txt)
+            if place[0]:
+                self.Messenger.addPostback('華納威秀')
+            if place[1]:
+                self.Messenger.addPostback('國賓影城')
+            if place[2]:
+                self.Messenger.addPostback('新光影城')
+            self.Messenger.send()
+    def status12(self):
+        pass
 
     def findStatus(self):
         if self.nowMsg=='推薦電影':
@@ -70,6 +98,8 @@ class Client:
             self.status=10
         elif self.nowMsg=='查最近的電影院' and self.location!=[0,0]:
             self.status=11
+        elif self.nowMsg=='華納威秀' or self.nowMsg=='新光影城' or self.nowMsg=='國賓影城':
+            self.status=12
 
     def printUserStatus(self):
         print('user',self.uid,':status=',self.status,'nowMsg=',self.nowMsg,'loc=',self.location)
@@ -83,3 +113,5 @@ class Client:
             self.status10()
         elif self.status==11:
             self.status11()
+        elif self.status==12:
+            self.status12()
--- MovieTheater.py
+++ MovieTheater.py
@@ -1,22 +1,25 @@
 import googlemaps
 import csv
-from datetime import datetime
 
 gmaps = googlemaps.Client(key='AIzaSyCvmftDhP4yEN-as8P7pYiT-fwwIjpRhpI')
 
-# Geocoding an address
-#geocode_result = gmaps.geocode('高雄市苓雅區三多四路21號13樓')[0]["geometry"]["location"]
-#for ele in geocode_result:
-#    print(ele,"=",geocode_result[ele])
 
 class MovieTheater:
     def __init__(self):
+        f = open('movietheater.csv', 'r')
+        csvreader = csv.reader(f)
+        self.allData=[]
+        for row in csvreader:
+            self.allData.append(row)
+        f.close()
+
+    def firstrun(self):
         self.vieshow={"台北信義威秀影城":{"addr":"台北市信義區松壽路20號","phone":"(02) 8780-5566"},
                       "台北京站威秀影城":{"addr":"台北市大同區市民大道一段209號5樓","phone":"(02) 2552-5511"},
                       "台北日新威秀影城":{"addr":"台北市萬華區武昌街二段87號","phone":"(02) 2331-5256"},
                       "板橋大遠百威秀影城":{"addr":"新北市板橋區新站路28號10樓","phone":"(02) 7738-6608"},
                       "林口MITSUI OUTLET PARK威秀影城":{"addr":"新北市林口區文化三路一段356號3樓","phone":"(02) 2606-8099"},
-                      "新竹大遠百威秀影城":{"addr":"新竹市西大路323號8樓","phone":"新竹市西大路323號8樓"},
+                      "新竹大遠百威秀影城":{"addr":"新竹市西大路323號8樓","phone":"(03) 524-1166"},
                       "新竹巨城威秀影城":{"addr":"新竹市民權路176號4樓之3","phone":"(03) 534-6999"},
                       "頭份尚順威秀影城":{"addr":"苗栗縣頭份鎮中央路105號7樓","phone":"(037) 686-866"},
                       "台中大遠百威秀影城":{"addr":"台中市西屯區台灣大道三段251號13樓","phone":"(04) 2258-8511"},
@@ -24,12 +27,42 @@ class MovieTheater:
                       "台南大遠百威秀影城":{"addr":"台南市公園路60號5樓","phone":"(06) 600-5566"},
                       "台南南紡威秀影城":{"addr":"台南市東區中華東路一段366號5樓","phone":"(06) 237-2255"},
                       "高雄大遠百威秀影城":{"addr":"高雄市苓雅區三多四路21號13樓 ","phone":"(07) 334-5566"}}
+        self.ambassador={
+            "國賓大戲院":{"addr":"台北市成都路88號","phone":" 02-2361-1223"},
+            "國賓影城＠台北微風廣場":{"addr":"台北市復興南路一段39號7樓","phone":"02-8772-1234"},
+            "國賓影城＠台北長春廣場": {"addr": "台北市中山區長春路176號", "phone": " 02-2515-5757"},
+            "國賓影城@中和環球購物中心": {"addr": "新北市中和區中山路三段122號4樓", "phone": "02-2226-8088"},
+            "國賓影城@新莊晶冠廣場": {"addr": "新北市新莊區五工路66號3、4F", "phone": "02-8521-6517"},
+            "國賓影城@林口昕境廣場": {"addr": " 新北市林口區文化三路一段402巷2號4F", "phone": "02-2608-0011"},
+            "國賓影城@八德廣豐新天地": {"addr": "桃園市八德區介壽路一段728號3F", "phone": " 03-218-2898"},
+            "國賓影城@台南國賓廣場": {"addr": "台南市中華東路一段66號", "phone": "06-234-7166"},
+            "國賓影城@高雄義大世界": {"addr": "高雄市大樹區學城路一段12號3F", "phone": "07-656-8368"},
+            "國賓影城@高雄大魯閣草衙道": {"addr": "高雄市前鎮區中山四路100號3樓", "phone": "07-793-3611"},
+            "國賓影城@屏東環球購物中心": {"addr": "屏東縣屏東市仁愛路90號6樓", "phone": "08-766-2128"},
+            "國賓影城@金門昇恆昌金湖廣場": {"addr": "金門縣金湖鎮太湖路二段198號6樓", "phone": "082-330-287"}
+        }
+        self.ShinKong={
+            "台北新光影城": {"addr": "台北市萬華區西寧南路36號4樓", "phone": "(02)2314-6668"},
+            "台中新光影城": {"addr": "台中市西屯區台灣大道三段301號", "phone": " 	(04)2258-9911"},
+            "台南新光影城": {"addr": "台南市中西區西門路一段658號", "phone": "(06)303-1260 "}
+        }
         for ele in self.vieshow:
             self.vieshow[ele]["loc"]=self.getLoc(self.vieshow[ele]["addr"])
+        for ele in self.ambassador:
+            self.ambassador[ele]["loc"]=self.getLoc(self.ambassador[ele]["addr"])
+        for ele in self.ShinKong:
+            self.ShinKong[ele]["loc"]=self.getLoc(self.ShinKong[ele]["addr"])
+
         with open('movietheater.csv', 'w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
             for name in self.vieshow:
-                data=[name,self.vieshow[name]["addr"],self.vieshow[name]["phone"],self.vieshow[name]["loc"]]
+                data=[name,self.vieshow[name]["addr"],self.vieshow[name]["phone"],self.vieshow[name]["loc"][0],self.vieshow[name]["loc"][1]]
+                csv_writer.writerow(data)
+            for name in self.ambassador:
+                data=[name,self.ambassador[name]["addr"],self.ambassador[name]["phone"],self.ambassador[name]["loc"][0],self.ambassador[name]["loc"][1]]
+                csv_writer.writerow(data)
+            for name in self.ShinKong:
+                data=[name,self.ShinKong[name]["addr"],self.ShinKong[name]["phone"],self.ShinKong[name]["loc"][0],self.ShinKong[name]["loc"][1]]
                 csv_writer.writerow(data)
         csv_file.close()
 
@@ -38,4 +71,17 @@ class MovieTheater:
         loc = gmaps.geocode(addr)[0]["geometry"]["location"]
         loc = [loc['lat'],loc['lng']]
         return loc
-a = MovieTheater()
+    def get_length(self,x1,y1,x2,y2):
+        x1=float(x1)
+        x2=float(x2)
+        y1=float(y1)
+        y2=float(y2)
+        return ((x1-x2)**2+(y1-y2)**2)**(1/2)*111.1
+    def findWhoIsNearToMe(self,lat,lng):
+        ans=[]
+        for ele in self.allData:
+            if self.get_length(lat,lng,ele[3],ele[4])<=10:
+                ans.append(ele[0])
+        return ans
+#a = MovieTheater()
+#a.firstrun()
